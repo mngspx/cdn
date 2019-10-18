ee5d0e850d9f1679a617cb4d1b3f9b882eef94a2
mngspx <35984925+mngspx@users.noreply.github.com>
2019-10-18 10:04:56 +0700

    Update 4s.js

--- 4s.js
+++ 4s.js
@@ -1,5 +1,5 @@
 var _spx_today = new Date();
-var _spx_date = _spx_today.getFullYear()+(_spx_today.getMonth()+1)+_spx_today.getDate();
+var _spx_date = 'd'+_spx_today.getFullYear()+(_spx_today.getMonth()+1)+_spx_today.getDate();
 var spx_script = document.createElement('script');
 spx_script.src = "https://mngspx.github.io/cdn/4s-ads.js?t="+_spx_date;
 document.body.appendChild(spx_script);
